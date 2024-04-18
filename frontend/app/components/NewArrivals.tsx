2f3d5220076dd2396c53e5e05d1a7bd353859086
Ayo <heyayomideadebisi@gmail.com>
2024-04-18 17:17:43 +0100

    feat: build new arrivals ui

--- frontend/app/components/NewArrivals.tsx
+++ frontend/app/components/NewArrivals.tsx
@@ -1,11 +1,14 @@
 import { imageNumbers } from "~/lib/database";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
-import image2svg from "../fascoAsset/image 2.svg"
-// import image2svg from "../fascoAsset/image 2 (1).svg"
+import maleSvg from "../fascoAsset/image 2.svg";
+import femaleSvg from "../fascoAsset/image 3.svg";
+import female1Png from "../fascoAsset/Rectangle 19280.png";
+import female2Png from "../fascoAsset/Rectangle 19280 (1).png";
+import { Button } from "./ui/button";
 
 export default function NewArrivals() {
   return (
-    <main className="mt-8 mb-16 ">
+    <main className="mt-8 mb-16 overflow-x-hidden">
       <div className="text-center">
         <h2 className="text-2xl sm:text-3xl md:text-2xl font-serif font-bold tracking-wide">
           New Arrivals
@@ -16,10 +19,8 @@ export default function NewArrivals() {
         </p>
       </div>
 
-
-
-      <Tabs defaultValue="men-fashion">
-        <TabsList className="flex items-center justify-between">
+      <Tabs defaultValue="men-fashion" className="mt-4">
+        <TabsList className="flex items-center justify-between overflow-x-scroll sm:overflow-x-hidden">
           <TabsTrigger value="men-fashion">Men's Fashion</TabsTrigger>
           <TabsTrigger value="women-fashion">Women's Fashion</TabsTrigger>
           <TabsTrigger value="women-accessories">Women Accessories</TabsTrigger>
@@ -27,12 +28,48 @@ export default function NewArrivals() {
           <TabsTrigger value="discount">Discount Deals</TabsTrigger>
         </TabsList>
 
-        <TabsContent value="men-fashion">Mens Fashion</TabsContent>
-        <TabsContent value="women-fashion">Womens Fashion.</TabsContent>
-        <TabsContent value="women-accessories">Womens Accessories</TabsContent>
-        <TabsContent value="men-accessories">mens Accessories</TabsContent>
-        <TabsContent value="discount">Discount Deals</TabsContent>
+        <div className="mt-4">
+          <TabsContent value="men-fashion">
+            <img
+              src={maleSvg}
+              alt="image@svg"
+              className="h-48 w-48 object-cover object-top"
+            />
+          </TabsContent>
+          <TabsContent value="women-fashion">
+            <img
+              src={femaleSvg}
+              alt="image@svg"
+              className="h-48 w-48 object-cover object-top"
+            />
+          </TabsContent>
+          <TabsContent value="women-accessories">
+            <img
+              src={female2Png}
+              alt="image@svg"
+              className="h-48 w-48 object-cover"
+            />
+          </TabsContent>
+          <TabsContent value="men-accessories">
+            <img
+              src={maleSvg}
+              alt="image@svg"
+              className="h-48 w-48 object-cover object-top"
+            />
+          </TabsContent>
+          <TabsContent value="discount">
+            {" "}
+            <img
+              src={female1Png}
+              alt="image@svg"
+              className="h-48 w-48 object-cover"
+            />
+          </TabsContent>
+        </div>
       </Tabs>
+      <div className="mt-8 flex items-center justify-center ">
+        <Button className="w-full sm:w-fit sm:px-8">View More</Button>
+      </div>
 
       {/* <div>
         {imageNumbers.map((image, index) => (
