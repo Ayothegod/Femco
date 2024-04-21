cf2066e0f637852da31b6db24b6e956b03c1f1f7
Ayo <heyayomideadebisi@gmail.com>
2024-04-21 11:27:46 +0100

    build: add appwrite

---
+++ frontend/app/lib/appwrite.ts
@@ -0,0 +1,8 @@
+// import { Client } from 'appwrite';
+// const client = new Client();
+
+// client
+//     .setEndpoint('https://cloud.appwrite.io/v1')
+//     .setProject('6624e6c59fa50e4ba0c3');
+
+// 6624e6c59fa50e4ba0c3
