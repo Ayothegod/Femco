e549f4c1161ae3b029237f95da537478ef70f315
Ayo <heyayomideadebisi@gmail.com>
2024-04-22 14:04:27 +0100

    chore: yesterdays last commit

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,14 +1,21 @@
 import { navLinks } from "~/lib/database";
 import { Button } from "./ui/button";
+import { Link, useLocation } from "@remix-run/react";
 
-export default function Header({ auth }: { auth: string }) {
+export default function Header() {
+  const location = useLocation();
+  const path = location.pathname;
+  const user = false;
+
   return (
     <header className="pageStyle flex items-center justify-between py-6">
-      <h1 className="text-2xl sm:text-3xl md:text-4xl font-serif font-bold tracking-wide">
-        FASCO
-      </h1>
+      <Link to={`${user ? "/" : "/login"}`}>
+        <h1 className="text-2xl sm:text-3xl md:text-4xl font-serif font-bold tracking-wide">
+          FASCO
+        </h1>
+      </Link>
 
-      {!auth && (
+      {path !== "/login" && path !== "/register" && (
         <nav className="flex items-center gap-8">
           <ul className="hidden md:flex items-center gap-8">
             {navLinks.map((link, idx) => (
--- frontend/app/routes/login/route.tsx
+++ frontend/app/routes/login/route.tsx
@@ -1,6 +1,10 @@
+import Header from "~/components/Header";
 
-export default function SigninRoute() {
+export default function LoginRoute() {
   return (
-    <div>SigninRoute</div>
-  )
+    <main>
+      <div>LoginRoute</div>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, sequi. Quis quisquam voluptate doloribus, reiciendis dignissimos officia consequuntur impedit dolore.</p>
+    </main>
+  );
 }
