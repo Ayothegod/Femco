7bf92daad8fdbcf7b6c5595f5ae56d50e18d6047
Ayo <heyayomideadebisi@gmail.com>
2024-05-02 21:07:04 +0100

    build: init shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Fasco Ecommerce</h1>
+      <Button size="lg">Hello Baby, how do you do</Button>
     </main>
   );
 }
